bc96964d6c5cb8fabb488e03d6d9862e02380dc2
Vivek386 <vivekkumarmahto1998@gmail.com>
2024-11-14 02:00:16 +0530

    pls check now

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: ''),
+      home: const MyHomePage(title: 'Rohan Dhakkar'),
     );
   }
 }
